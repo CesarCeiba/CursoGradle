c9f2d763a066848ae960b25ca28725718d0f028b
CEIBA\cesar.velasquez <cesar.velasquez@ceiba.com.co>
2018-01-18 15:39:10 -0500

    Dañando Test

--- proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -35,7 +35,7 @@ public class NotifyPersonServiceTest {
 		//Act
 		String message = notifyPersonService.notify(person);		
 		//Assert
-		Assert.assertNotNull(message);
+		Assert.assertNull(message);
 		System.out.println(message);
 	}
 }
